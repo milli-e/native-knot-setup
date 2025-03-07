e2a0601ad39bd743473ebfd6d9b90e7a99b463f8
milli-e <163801884+milli-e@users.noreply.github.com>
2025-03-07 11:41:51 +0900

    script edit

--- native-knot-setup.sh
+++ native-knot-setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 프로젝트 이름 입력
-read -p "📄 프로젝트 이름을 입력해주세요 (기본값: MyReactNativeApp): " PROJECT_NAME
+read -p "📄 프로젝트 이름을 입력해주세요 (기본값: MyReactNativeApp): " PROJECT_NAME </dev/tty
 PROJECT_NAME="${PROJECT_NAME:-MyReactNativeApp}" # 기본값 설정
 
 # React Native 버전 입력
-read -p "👉 사용할 React Native 버전을 입력하세요 (기본값: 0.77): " RN_VERSION
+read -p "👉 사용할 React Native 버전을 입력하세요 (기본값: 0.77): " RN_VERSION </dev/tty
 RN_VERSION="${RN_VERSION:-0.77}" # 기본값 설정
 
 # 프로젝트 생성
